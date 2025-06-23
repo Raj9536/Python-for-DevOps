5ab925b634005db40231a0cf68eaa4b8cd935a15
Raj9536 <rajmourya9536@gmail.com>
2025-06-23 07:57:37 +0530

    function

---
+++ Functions.py
@@ -0,0 +1,32 @@
+#without using function:
+# a = 5
+# b = 10
+
+# Add = a + b
+# print(Add)
+
+# Sub = a - b
+# print(Sub)
+
+# mul = a * b
+# print(mul)
+
+#with function
+a=5
+b=10
+
+def add():
+    add = a+b
+    print(add)
+
+def sub():
+    s= a-b     
+    print(s)
+
+def mul():
+    m= a*b
+    print(m)
+
+add()
+sub()
+mul()        
